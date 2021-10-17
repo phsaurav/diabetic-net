5dcb2fc516fe1027c3ed5c505a2634299be4f2a3
PH Saurav <phsaurav29@gmail.com>
2021-10-18 01:57:43 +0600

    Basic Initialization Complete ⚓

---
+++ src/containers/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div className="mt-20 text-6xl text-center">
+			<h1>404 Page NotFound!!</h1>
+		</div>
+	);
+};
+
+export default NotFound;
