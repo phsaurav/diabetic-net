ed5ffd5a7ff0a3705479937e18a7b5fd3e96ea7d
PH Saurav <phsaurav29@gmail.com>
2021-10-19 23:46:09 +0600

    Subscribe Added 🏆

--- src/containers/Subscribe/Subscribe.js
+++ src/containers/Subscribe/Subscribe.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Subscribe = () => {
 	return (
 		<div>
-			<h1>Thank you For Subscribing</h1>
+			<div className="h-screen w-full flex flex-col items-center justify-center bg-teal-lightest font-sans">
+				<div
+					v-if="modal.visible"
+					className="h-screen w-full absolute flex items-center justify-center bg-modal mb-40"
+				>
+					<div className="bg-white rounded shadow p-8 m-4 max-w-xs max-h-full text-center ">
+						<h1
+							className="text-center text-3xl font-bold text-blue-40 py-5  uppercase"
+							id="services"
+						>
+							Thanks for Subscribing
+						</h1>
+						<Link to="/home">
+							<div className=" mt-3  flex  flex-row justify-center">
+								<button
+									className=" p-2  w-40  bg-brand-1  rounded-lg  text-white hover:bg-indigo-300"
+									type="button"
+								>
+									Go Back
+								</button>
+							</div>
+						</Link>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
