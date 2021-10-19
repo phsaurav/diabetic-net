d55c17ba3f291932930a7d7bb7f9db8ef2e05898
PH Saurav <phsaurav29@gmail.com>
2021-10-19 22:21:34 +0600

    Blogs Complete

--- src/containers/Blogs/Blogs.js
+++ src/containers/Blogs/Blogs.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import useBlogs from '../../hooks/useBlogs';
+import Blog from './Blog/Blog';
+import { Switch, Route } from 'react-router-dom';
+import Card from './Card/Card';
 
 const Blogs = () => {
+	const [blogs] = useBlogs();
 	return (
 		<div>
-			<h1>This is Blogs!!</h1>
+			<div className="px-6 py-8 bg-blue-10 min-h-screen">
+				<div className="container flex justify-between mx-auto">
+					<div className="w-full lg:w-8/12">
+						<div className="flex items-center justify-between">
+							<h1 className="text-xl font-bold text-gray-700 md:text-3xl">
+								Posts
+							</h1>
+						</div>
+						<Switch>
+							<Route path="/blogs" exact>
+								{blogs.map((blog) => (
+									<Card key={blog.key} blog={blog}></Card>
+								))}
+							</Route>
+							<Route path="/blogs/:key">
+								<Blog blogs={blogs}></Blog>
+							</Route>
+						</Switch>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
